4736b712b3530879e82be207e410622af0f566d8
Jessica Jones <daddehjones@gmail.com>
2021-06-14 18:04:07 -0400

    add open account method

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,8 +1,16 @@
 class Bank
 
-  attr_reader :name
-  
-  def initialize(name)
-    @name = name
+  attr_reader :bank_name
+  attr_accessor :bank_accounts
+
+  def initialize(bank_name)
+    @bank_name = bank_name
+    @bank_accounts = []
+  end
+
+  def open_account(person)
+  @bank_accounts << person
+  p  "An account has been opened for #{person.name} with #{bank_name}."
+
   end
 end
